1853f47c0db726fa4498e7f60021c1e1fed8b4ad
Puig99 <32305680+Puig99@users.noreply.github.com>
2018-03-04 13:09:32 +0100

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -5,7 +5,6 @@ var respuestaSMultiple1 = [];
 var respuestaSMultiple2 = [];
 var respuestaText1= null;
 var respuestaText2 = null;
-var respuestaText3= null;
 var respuestaRadio1 = [];
 var respuestaRadio2 = [];
 var respuestasCheckbox1 = [];
@@ -39,98 +38,82 @@ window.onload=function(){
 		//Text
 		var tituloInput1=xmlDoc.getElementsByTagName("title")[0].innerHTML;
 		ponerDatosInputHtml1(tituloInput1);
-		respuestaText1=xmlDoc.getElementById("profe001").getElementsByTagName("answer")[0];
+		respuestaText=xmlDoc.getElementById("profe001").getElementsByTagName("answer")[0];
 		
 		
 		var tituloInput2=xmlDoc.getElementsByTagName("title")[1].innerHTML;
 		ponerDatosInputHtml2(tituloInput2);
 		respuestaText2 = xmlDoc.getElementById("profe002").getElementsByTagName("answer")[1];
-		
-		var tituloInput3=xmlDoc.getElementsByTagName("title")[2].innerHTML;
-		ponerDatosInputHtml3(tituloInput3);
-		respuestaText3=xmlDoc.getElementById("profe003").getElementsByTagName("answer")[2];
-		
 	
 		
 	
 		//Select
-		var tituloSelect1=xmlDoc.getElementsByTagName("title")[3].innerHTML;
+		var tituloSelect1=xmlDoc.getElementsByTagName("title")[2].innerHTML;
 		var opcionesSelect1= [];
-		var nopt=xmlDoc.getElementById("profe004").getElementsByTagName('option').length;
+		var nopt=xmlDoc.getElementById("profe003").getElementsByTagName('option').length;
 			for (i = 0; i < nopt; i++){
-				opcionesSelect1[i] = xmlDoc.getElementById("profe004").getElementsByTagName('option')[i].innerHTML;
+				opcionesSelect1[i] = xmlDoc.getElementById("profe003").getElementsByTagName('option')[i].innerHTML;
 			}
 			ponerDatosSelectHtml1(tituloSelect1,opcionesSelect1);
-			respuestaSelect1=parseInt(xmlDoc.getElementsByTagName("answer")[3].innerHTML);
+			respuestaSelect1=parseInt(xmlDoc.getElementsByTagName("answer")[2].innerHTML);
 			
 			
-		var tituloSelect2=xmlDoc.getElementsByTagName("title")[4].innerHTML;
+		var tituloSelect2=xmlDoc.getElementsByTagName("title")[3].innerHTML;
 		var opcionesSelect2= [];
-		var nopt=xmlDoc.getElementById("profe005").getElementsByTagName('option').length;
+		var nopt=xmlDoc.getElementById("profe004").getElementsByTagName('option').length;
 			for (i = 0; i < nopt; i++){
-				opcionesSelect2[i] = xmlDoc.getElementById('profe005').getElementsByTagName('option')[i].innerHTML;
+				opcionesSelect2[i] = xmlDoc.getElementById('profe004').getElementsByTagName('option')[i].innerHTML;
 			}
 			ponerDatosSelectHtml2(tituloSelect2,opcionesSelect2);
-			respuestaSelect2=parseInt(xmlDoc.getElementsByTagName("answer")[4].innerHTML);
-		
-		var tituloSelect3=xmlDoc.getElementsByTagName("title")[5].innerHTML;
-		var opcionesSelect3= [];
-		var nopt=xmlDoc.getElementById("profe006").getElementsByTagName('option').length;
-			for (i = 0; i < nopt; i++){
-				opcionesSelect3[i] = xmlDoc.getElementById('profe006').getElementsByTagName('option')[i].innerHTML;
-			}
-			ponerDatosSelectHtml3(tituloSelect3,opcionesSelect3);
-			respuestaSelect3=parseInt(xmlDoc.getElementsByTagName("answer")[5].innerHTML);
-		
+			respuestaSelect2=parseInt(xmlDoc.getElementsByTagName("answer")[3].innerHTML);
 		
 		//Select multiple
 	
 		
-		var tituloSMultiple1=xmlDoc.getElementsByTagName("title")[6].innerHTML;
+		var tituloSMultiple1=xmlDoc.getElementsByTagName("title")[4].innerHTML;
 		var opcionesSMultiple1=[];
-		var nopt=xmlDoc.getElementById("profe007").getElementsByTagName('option').length;
+		var nopt=xmlDoc.getElementById("profe005").getElementsByTagName('option').length;
 			for (i=0; i<nopt; i++){
 				opcionesSMultiple1[i]=xmlDoc.getElementsByTagName('option')[i].innerHTML;
 			}
 		ponerDatosSMultipleHtml1(tituloSMultiple1,opcionesSMultiple1);
-		respuestaSMultiple1=parseInt(xmlDoc.getElementsByTagName("answer")[6].innerHTML);
+		respuestaSMultiple1=parseInt(xmlDoc.getElementsByTagName("answer")[4].innerHTML);
 			
-		var tituloSMultiple2=xmlDoc.getElementsByTagName("title")[7].innerHTML;
+		var tituloSMultiple2=xmlDoc.getElementsByTagName("title")[5].innerHTML;
 		var opcionesSMultiple2=[];
-		var nopt=xmlDoc.getElementById("profe008").getElementsByTagName('option').length;
+		var nopt=xmlDoc.getElementById("profe006").getElementsByTagName('option').length;
 			for (i=0; i<nopt; i++){
-				opcionesSMultiple2[i]=xmlDoc.getElementById("profe008").getElementsByTagName('option')[i].innerHTML;
+				opcionesSMultiple2[i]=xmlDoc.getElementById("profe006").getElementsByTagName('option')[i].innerHTML;
 			}
 		ponerDatosSMultipleHtml2(tituloSMultiple2,opcionesSMultiple2);
-		respuestaSMultiple2=parseInt(xmlDoc.getElementsByTagName("answer")[7].innerHTML);
+		respuestaSMultiple2=parseInt(xmlDoc.getElementsByTagName("answer")[5].innerHTML);
 		
 		//checkbox
-		var tituloCheckbox1=xmlDoc.getElementsByTagName("title")[8].innerHTML;
+		var tituloCheckbox1=xmlDoc.getElementsByTagName("title")[6].innerHTML;
 		var opcionesCheckbox1 = [];
-		var nopt =xmlDoc.getElementById("profe009").getElementsByTagName('option').length;
+		var nopt =xmlDoc.getElementById("profe007").getElementsByTagName('option').length;
 			for (i=0; i< nopt; i++){
-				opcionesCheckbox1[i]=xmlDoc.getElementById("profe009").getElementsByTagName('option')[i].innerHTML;
+				opcionesCheckbox1[i]=xmlDoc.getElementById("profe007").getElementsByTagName('option')[i].innerHTML;
 			}
 			ponerDatosCheckboxHtml1(tituloCheckbox1,opcionesCheckbox1);
-			var nres=xmlDoc.getElementById("profe009").getElementsByTagName('answer').length;
+			var nres=xmlDoc.getElementById("profe007").getElementsByTagName('answer').length;
 				for (i=0; i< nres; i++){
-					respuestasCheckbox1[i]=parseInt(xmlDoc.getElementById("profe009").getElementsByTagName("answer")[i].innerHTML);
+					respuestasCheckbox1[i]=parseInt(xmlDoc.getElementById("profe007").getElementsByTagName("answer")[i].innerHTML);
 				}
 		
 		
 		//radio
-		var tituloRadio1=xmlDoc.getElementsByTagName("title")[9].innerHTML;
+		var tituloRadio1=xmlDoc.getElementsByTagName("title")[8].innerHTML;
 		var opcionesRadio1=[];
-		var nopt=xmlDoc.getElementById("profe010").getElementsByTagName('option').length;
+		var nopt=xmlDoc.getElementById("profe009").getElementsByTagName('option').length;
 			for (i = 0; i < nopt; i++){
-				opcionesRadio1[i]= xmlDoc.getElementById("profe010").getElementsByTagName("option")[i].innerHTML;
+				opcionesRadio1[i]= xmlDoc.getElementById("profe009").getElementsByTagName("option")[i].innerHTML;
 			}
 			ponerDatosRadioHtml1(tituloRadio1,opcionesRadio1);
-			var nres=xmlDoc.getElementById("profe010").getElementsByTagName("answer").length;
+			var nres=xmlDoc.getElementById("profe009").getElementsByTagName("answer").length;
 				for (i=0; i< nres; i++){
-						respuestaRadio1[i]=xmlDoc.getElementById("profe010").getElementsByTagName("answer")[i].innerHTML;
+						respuestaRadio1[i]=xmlDoc.getElementById("profe009").getElementsByTagName("answer")[i].innerHTML;
 				}
-	}
 				
 	
 	
@@ -141,10 +124,6 @@ window.onload=function(){
 	
 	function ponerDatosInputHtml2(t){
 		document.getElementById("tituloInput2").innerHTML=t;
-	}
-		
-		function ponerDatosInputHtml3(t){
-		document.getElementById("tituloInput3").innerHTML=t;
 	}
 	
 	function ponerDatosSelectHtml1(t,opt){
@@ -169,18 +148,7 @@ window.onload=function(){
 		}
 	}
 	
-		function ponerDatosSelectHtml3(t,opt){
-		document.getElementById("tituloSelect3").innerHTML=t;
-		var select = document.getElementsByTagName("select")[2];
-		for (i=0; i< opt.length; i++){
-			var option = document.createElement("option");
-			option.text= opt[i];
-			option.value= i+1;
-			select.options.add(option);
-		}
-	}
 }
-		
 
 	function ponerDatosSMultipleHtml1(t,opt){
 		document.getElementById("tituloSMultiple1").innerHTML=t;
@@ -240,4 +208,4 @@ window.onload=function(){
    			 radioContainer1.appendChild(document.createElement("br"));
  }  
 }
-				
+}
